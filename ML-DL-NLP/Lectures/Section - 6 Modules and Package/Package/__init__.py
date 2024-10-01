998f2a66bb5473c9b644c8b2de42740dd3ec12bb
Harsh Sinha <sinha.harshsep@gmail.com>
2024-10-01 12:47:04 +0530

    Others

--- ML-DL-NLP/Lectures/Section - 6 Modules and Package/Package/__init__.py
+++
@@ -1,8 +0,0 @@
-"""This is a special file that tells Python that this directory
-should be considered a Python package."""
-
-"""___init__.py is a special Python file that is used to define a 
-package and initialize their namespaces in Python."""
-
-"""The __init__.py file is usually kept empty, but can be used too"""
-"""Need to keep it in the directory to make it a package"""
